4bc86f37d60d750cc1dc6ae433eb80d54eddca54
longminhdo <longminhdo2000@gmail.com>
2023-11-10 12:02:01 +0700

    vendor: update webpack for deployment

--- craco.config.js
+++ craco.config.js
@@ -23,33 +23,5 @@ Object.keys(alias).forEach((k) => {
 module.exports = {
   webpack: {
     alias,
-    plugin: {
-      add: [
-        new Dotenv(),
-        new DefinePlugin({
-          'process.env': {
-            REACT_APP_ENV: JSON.stringify(process.env.REACT_APP_ENV),
-          },
-        }),
-      ],
-    },
-    configure: (webpackConfig) => {
-      console.log('Start bundling...');
-      console.log('webpackConfig can be logged out here');
-      return {
-        ...webpackConfig,
-        entry: './src/index.tsx',
-        output: {
-          ...webpackConfig.output,
-          path: '/Users/pboy208/Documents/GR/hust-form/form-portal/build',
-          pathinfo: process.env.REACT_APP_ENV !== 'staging',
-          filename: 'static/js/[name].[contenthash:8].js',
-          chunkFilename: 'static/js/[name].[contenthash:8].chunk.js',
-          assetModuleFilename: 'static/media/[name].[hash][ext]',
-          publicPath: process.env.REACT_APP_ENV === 'staging' ? './' : '/',
-          library: 'FormHust',
-        },
-      };
-    },
   },
 };
